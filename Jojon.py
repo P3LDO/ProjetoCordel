1e1538f5b4b48922471f62b7d2435b5641c4987a
P3LDO <pedrokayro2006@gmail.com>
2024-03-14 20:56:39 -0300

    UPDATE_8(FF)

---
+++ Jojon.py
@@ -0,0 +1,69 @@
+while True:
+ valor1=input('vc é gay?       _')
+ if valor1 == 'sim':
+   print('eu ja sabia')
+ elif valor1 == 'sou':
+   print('eu ja sabia, gay')
+ elif 'mario' in valor1:
+   print('o mario q te comeu atrás do armario')
+ elif 'Goku' in valor1:
+   print('sinto muito, Akira Toriyama')
+ elif 'dragon ball' in valor1:
+   print('seu sorriso é tão resplandecente, q deixou meu coração alegre...')
+ elif 'one piece' in valor1:
+   print('one piece is real!!!')
+ elif valor1 == 'Sou':
+   print('para de ser gay mano')
+ elif valor1 == 'Sim':
+   print('eu vou te matar')
+ elif valor1 == 'sou sim':
+   print('eu sou homofóbico')
+ elif valor1 == 'talvez':
+   print('talvez, não, vc É gay')
+ elif valor1 == 'Talvez':
+   print('vc com crtz é gay')
+ elif valor1 == 'vc é':
+   print('eu não, vc q da a bunda')
+ elif valor1 == 'Vc é':
+   print('vc é gayzão')
+ elif valor1 == 'ss':
+   print('vdd, vc é')
+ elif valor1 == 'o João':
+   print('ele n, só vc')
+ elif 'joao' in valor1:
+   print('João é com letra maiúscula, gay analfabeto')
+ elif 'israel' in valor1:
+    print('o japonês é gay demais')
+ elif 'Israel'in valor1:
+    print('o asiático é mesmo')
+ elif 'ISRAEL' in valor1:
+    print('tá tentatando burlar o código? gay')
+ elif 'Joao' in valor1:
+    print('e o acento? gay burro')
+ elif 'João' in valor1:
+    print('n fala dele, burro gay')
+ elif 'JOAO' in valor1:
+    print('tá tentando burlar, né? gay!!!')
+ elif 'JOÃO' in valor1:
+    print('para de tentar burlar, gayzão')
+ elif 'nn' in valor1:
+    print('vc é gay e burro')
+ elif 'não' in valor1:
+    print('vc é extremamente gay, bixa')
+ elif 'teu' in valor1:
+    print('desiste, gay')
+ elif 'seu' in valor1:
+    print('para de tentar, vc é gay demais pra ganhar do programa')
+ elif valor1 == (''):
+     print('é pra escrever alguma coisa, idiota')
+ elif 'n' in valor1:
+    print('vc é tipo um super gay, daqueles q vai em uma orgia gay')
+ elif valor1.isalpha() == True:
+    print('vc faz amor com o bumbum, mano')
+ elif valor1.isnumeric() == True:
+    print('número não, imbecil')
+ elif valor1.isalnum() == True:
+    print('vc faz amor com o bumbum, mano')
+ else:
+    print('escreve algo')
+ continue
